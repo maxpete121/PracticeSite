fdcccc3217b06e781c44193340336d5f1ebe6c09
maxpete121 <maxpete121@gmail.com>
2023-12-21 16:32:36 -0700

    update draw

--- app/controllers/ItemsController.js
+++ app/controllers/ItemsController.js
@@ -1,9 +1,10 @@
 import { AppState } from "../AppState.js"
 import { Items } from "../models/Items.js"
 import { itemService } from "../services/ItemsService.js"
+import { Pop } from "../utils/Pop.js"
 
-function _drawItems(){
-    const items = AppState.Item
+function _drawItemsATX(){
+    const items = AppState.Item.filter(item => item.buildType == 'ATX')
     let content = ''
     const itemDraw = items.forEach(item => content += item.itemTemplate)
     document.getElementById('item-view').innerHTML = content
@@ -13,7 +14,7 @@ export class ItemController{
     constructor(){
         console.log('Item controller loaded')
         this.getItems()
-        AppState.on('Item', _drawItems)
+        AppState.on('Item', _drawItemsATX)
     }
 
     async getItems(){
